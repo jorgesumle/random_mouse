a2040485a6fe5d6bafd3d401e66a113a82d1448b
Jorge <jorgesumle@gmail.com>
2016-06-16 11:52:24 +0200

    Update random_mouse.sh
    Corregida falta de ortografía.

--- random_mouse.sh
+++ random_mouse.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if test -z $1; then
-	echo 'Error no has introducido los segundos.'
+	echo 'Error, no has introducido los segundos.'
 	exit
 fi
 xRes=$(xrandr | fgrep '*' | cut -d" " -f4 | cut -d"x" -f1)
